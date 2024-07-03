949051184ea145a82d7f2b1bb14f4a7f315ea743
Lucielle-Voeffray <pro@lucielle.ch>
2024-07-03 15:08:19 +0200

    Ajout des mentions de copyright

--- src/main/sql/tumblrFeed.sql
+++ src/main/sql/tumblrFeed.sql
@@ -1,4 +1,24 @@
 
+--    Discord bot allowing the creation of Tumblr feeds
+--    Copyright (C) 2024  Lucielle Voeffray
+--
+--    This program is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    This program is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+--
+--    Contact:
+--        pro@lucielle.ch
+
+
 CREATE DATABASE IF NOT EXISTS db_TumblrFeed CHARACTER SET utf8mb4;
 
 CREATE TABLE IF NOT EXISTS t_server (
